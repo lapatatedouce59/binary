da43f7f0dae96984398355272a6be670d6c32564
lapatatedouce59 <momo29004@gmail.com>
2023-11-24 21:45:22 +0100

    V1 terminée

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ zoneGame.style.display='none';
 let zoneResults = document.getElementById('zoneResults')
 zoneResults.style.display='none';
 
+let resultTable = document.getElementById('resultTable')
+
 
 
 let targetNum = document.getElementById('targetNum')
@@ -19,9 +21,9 @@ let actualNum = document.getElementById('actualNum')
 
 let btnContainer = document.getElementById('btnContainer')
 
-maxNum.value=(2**parseInt(nbBits.value))
+maxNum.value=(2**parseInt(nbBits.value)-1)
 nbBits.addEventListener('input',()=>{
-    maxNum.value=(2**parseInt(nbBits.value))
+    maxNum.value=(2**parseInt(nbBits.value)-1)
 })
 
 let actualGame=false
@@ -29,9 +31,17 @@ let actualGame=false
 let hasGameStarted=false
 let actualRound=0
 
+let clicks = 0
+
+document.body.addEventListener('click',()=>{
+    clicks++
+},true)
+
 startGame.addEventListener('click',()=>{
     zoneGame.style.display='flex';
     zoneBtn.style.display='none';
+    nbRounds.disabled=true
+    nbBits.disabled=true
     hasGameStarted=true
     actualRound=1
     actualGame=new Game(parseInt(nbRounds.value),parseInt(nbBits.value))
@@ -45,11 +55,13 @@ class Game{
         this.Save=[]
         this.Times=[]
         this.TimeMoyen=0
+        this.ClicMoyen=0
+        this.Clics=[]
     }
 
     reloadUi(){
         btnContainer.innerHTML='';
-        for(let i = 0;i<=parseInt(nbBits.value);i++){
+        for(let i = 0;i<parseInt(nbBits.value);i++){
             let parentDiv=document.createElement('div')
             parentDiv.classList.add('chkValue')
             parentDiv.classList.add('chkCont')
@@ -76,6 +88,7 @@ class Game{
     }
 
     quest(){
+        clicks=0
         if(hasGameStarted===false) return;
         this.startTime = Date.now()
         this.reloadUi()
@@ -101,11 +114,15 @@ class Game{
             this.endTime = Date.now()
             let resTime = new Date(this.endTime-this.startTime)
 
-            this.Save.push({ round: actualRound, target: targetNum.innerText, time: resTime.getSeconds()})
+            this.Save.push({ round: actualRound, target: targetNum.innerText, time: resTime.getSeconds(), clicks: clicks})
             this.Times.push(resTime.getSeconds())
-            let max = 0
-            for(let num of this.Times) max = max+num;
-            this.TimeMoyen=parseFloat(max/this.Times.length)
+            this.Clics.push(clicks)
+            let maxTimes = 0
+            for(let num of this.Times) maxTimes = maxTimes+num;
+            this.TimeMoyen=parseFloat(maxTimes/this.Times.length)
+            let maxClics = 0
+            for(let num of this.Clics) maxClics = maxClics+num;
+            this.ClicMoyen=parseFloat(maxClics/this.Clics.length)
 
             if(actualRound<this.rounds){
                 actualRound++
@@ -116,12 +133,48 @@ class Game{
                 console.log(this.Save)
                 actualRound=0
                 hasGameStarted=false
+
+                zoneBtn.style.display='none';
+                zoneGame.style.display='none';
+                zoneResults.style.display='flex';
+                resultTable.innerHTML='<tr><th scope="col">Round</th><th scope="col">Valeur cible</th><th scope="col">Temps</th><th scope="col">Clics</th></tr>';
+
+                document.getElementById('averageTime').innerText=this.TimeMoyen.toFixed(2)
+                document.getElementById('averageClic').innerText=this.ClicMoyen.toFixed(2)
+                
+                for(let result of this.Save){
+                    let mastertr = document.createElement('tr')
+                    let td1 = document.createElement('td')
+                    td1.innerText=result.round
+                    let td2 = document.createElement('td')
+                    td2.innerText=result.target
+                    let td3 = document.createElement('td')
+                    td3.innerText=`${result.time}sec`
+                    let td4 = document.createElement('td')
+                    td4.innerText=`${result.clicks} clics`
+                    mastertr.appendChild(td1)
+                    mastertr.appendChild(td2)
+                    mastertr.appendChild(td3)
+                    mastertr.appendChild(td4)
+                    resultTable.appendChild(mastertr)
+                }
+
+                for(let chk of document.getElementsByClassName('resChk')){
+                    chk.checked=true
+                    chk.disabled=true
+                }
             }
-            //console.log(`gagné en ${resTime.getSeconds()}sec!`)
         }
     }
-
     generateRandomInt(){
         return Math.floor(Math.random() * (2**parseInt(nbBits.value)));
     }
 }
+
+document.getElementById('continueBtn').addEventListener('click',()=>{
+    nbRounds.disabled=false
+    nbBits.disabled=false
+    zoneBtn.style.display='flex';
+    zoneGame.style.display='none';
+    zoneResults.style.display='none';
+})
